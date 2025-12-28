7bdd4065355ce03a5aeffa13c52c712c01de8ddc
Theo Gustavsson <theojohnhenry@gmail.com>
2025-12-28 12:47:24 +0100

    Insane software engineering completion

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,67 @@ fn main() {
 async fn run_async() -> Result<(), String> {
     let (to_b, from_a) = mpsc::channel::<Packet>(8); // sends from a to b
     let (to_a, from_b) = mpsc::channel::<Packet>(8); // sends from b to a
-    let (ra, rb) = tokio::join!(task_a(to_b, from_b), task_b(from_a, to_a));
+    let (ra, rb) = tokio::join!(client_a(to_b, from_b), server_b(from_a, to_a));
     ra?;
     rb?;
     Ok(())
 }
 
-async fn task_a(
+async fn client_a(
     to_b: mpsc::Sender<Packet>,
     mut from_b: mpsc::Receiver<Packet>,
 ) -> Result<(), String> {
+    let mut tx = SAWSender::new(5, 3);
+    let timeout_duration = Duration::from_millis(200);
+
+    while !tx.done() {
+        //try send packet
+        let mut out_pkt = tx.start_seq();
+        loop {
+            println!("A: sending {:?}", out_pkt);
+            to_b.send(out_pkt.clone())
+                .await
+                .map_err(|_| "A: send error".to_string())?;
+
+            // wait for recieved packet
+            let Ok(opt) = timeout(timeout_duration, from_b.recv()).await else {
+                // timed out
+                // do something - retry 3 times
+                // on_timeout();
+                out_pkt = tx.on_timeout()?;
+                continue;
+            };
+            // didnt time out
+            let Some(inc_pkt) = opt else {
+                //didnt return a pkt ?
+                // do something - return error;
+                return Err("A: channel closed".to_string());
+            };
+            // all good, recieved ack
+            // on_ack(); validate ack, then break loop, and advance internal seq
+
+            println!("A: recieved {:?}", inc_pkt);
+            tx.on_ack(&inc_pkt)?;
+            break;
+        }
+    }
     Ok(())
 }
 
-async fn task_b(
+async fn server_b(
     mut from_a: mpsc::Receiver<Packet>,
     to_a: mpsc::Sender<Packet>,
 ) -> Result<(), String> {
     let mut rx = SAWReceiver::new();
-    while let Some(pkt) = from_a.recv().await {
-        match rx.on_packet(&pkt) {
-            ReceiverAction::Send(ack) => to_a
-                .send(ack)
-                .await
-                .map_err(|_e| "B:channel closed".to_string())?,
+    while let Some(inc_pkt) = from_a.recv().await {
+        println!("B: recieved {:?}", inc_pkt);
+        match rx.on_packet(&inc_pkt) {
+            ReceiverAction::Send(ack) => {
+                println!("B sending {:?}", ack);
+                to_a.send(ack)
+                    .await
+                    .map_err(|_e| "B:channel closed".to_string())?
+            }
             ReceiverAction::Ignore => {}
             ReceiverAction::Error(e) => return Err(e),
         }
